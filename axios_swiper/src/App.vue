7e6ca76fcfac05b3c4ce2617026fc07f3108fb54
Cookie <2497430769@qq.com>
2020-05-11 09:42:27 +0800

    [新增]新增swiper 导入 安装后在node_modules 即可import 'swiper/css/swiper.min.css'

---
+++ axios_swiper/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    this is app vue<hr>
+    <!-- <img src="@/assets/1.jpg" alt="1.jpg"> -->
+    <swiper> <img src="@/assets/1.jpg" alt="1.jpg"></swiper>
+    <swiper> <img src="@/assets/2.jpg" alt="2.jpg"></swiper>
+    <swiper> <img src="@/assets/3.jpg" alt="3.jpg"></swiper>
+    <swiper> <img src="@/assets/4.jpg" alt="4.jpg"></swiper>
+  </div>
+</template>
+
+<script>
+import swiper from '@/components/swiper'
+export default {
+  data() {
+    return {
+      swiperimg : [],
+     
+    }
+  },
+  methods: {
+    
+  },
+  comments:{
+    swiper
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ axios_swiper/src/components/swiper.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class='swiper-container'>  
+            <div class='swiper-wrapper'>
+            <div class="swiper-slide">
+                <slot></slot>
+            </div>
+            <div class="swiper-pagination"></div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-scrollbar"></div>
+            </div>
+    </div>
+</template>
+
+<script>
+import Swiper from 'swiper'
+// 注意导入文件swiper 的路劲。。。
+import 'swiper/css/swiper.min.css'
+// import 'swiper/dist/css/swiper.min.css'
+export default {
+  mounted() {
+   new Swiper('.swiper-container', {
+      autoplay: true,    //自动播放
+      loop: true,        //循环播放
+      delay: 3000,       //每张图间隔三秒
+      //分页器
+      pagination: {
+        el: ".swiper-pagination"
+      },
+      //左右前进后退按钮
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      }
+    });
+  },
+    // mounted() {
+    //     new Swiper('.ditle',{
+    //         autoplay: true,    //自动播放
+    //         loop:true,  //循环播放
+    //         freeMode: true,
+    //         delay: 3000,       //每张图间隔三秒
+    //         // 这是分页器的一个初始化，使用时才初始化
+    //         pagination:{
+    //             el: '.swiper-pagination',
+    //         },
+    //         //左右前进后退按钮
+    //         navigation: {
+    //             nextEl: ".swiper-button-next",
+    //             prevEl: ".swiper-button-prev"
+    //         }
+    //     })
+    // }
+
+    // name : "cookie_swiper",
+    // data() {
+    //     return {
+            
+    //     }
+    // },
+
+}
+</script>
+
+
+<style lang="scss" scoped>
+.swiper-slide {
+        width: 100%;
+        height: 100%;
+        box-sizing: border-box;
+        // padding: 0 0.8rem;
+        img {
+          width: 100%;
+          height: 100%;
+        }
+      }
+</style>
